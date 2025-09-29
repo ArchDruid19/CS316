92b04f528ae3e2175ba2710f9d0519af9a803c24
ArchDruid19 <druidvester@gmail.com>
2025-09-29 00:11:41 -0400

    Finished HMWRK 2

--- TreeFun/src/com/druid/tree/Main.java
+++ TreeFun/src/com/druid/tree/Main.java
@@ -7,29 +7,43 @@ import com.druid.tree.BinaryTree.TraversalType;
 
 public class Main {
     public static void main(String[] args) {
-        BinaryTree test_tree = new BinaryTree();
-        Integer[] numbers = {23, 10, 30, 5, 15, 25, 40, 1, 8};
-
-        for (int i = 0; i < numbers.length; i++) {
-            test_tree.insert(numbers[i], TraversalMode.ITERATIVE, test_tree.getRoot());
-        }
-
-        System.out.println(test_tree.printTree(test_tree.root, TraversalType.LVR));
-
-        for (Integer i = test_tree.getMinTreeValue(), j = 0; i < test_tree.getMaxTreeValue() + 1; i ++) {
-            if (j % 10 == 0) {
-                System.out.println();
-            }
-            System.out.printf("%-5s ", test_tree.searchTree(i, TraversalMode.RECURSIVE, test_tree.getRoot()));
-            j++;
-        }
+        BinaryTree test_tree = getTreeFromInput();
+        printAllOptions(test_tree);
     }
 
     public static void printAllOptions(BinaryTree tree) {
-        for (TraversalType i: TraversalType.values()) {
+        // Traverses the tree in all 6 differant ways 
+        for (TraversalType i : TraversalType.values()) {
             System.out.println(i);
-            System.out.println(tree.printTree(tree.getRoot(), i));
+            System.out.println(tree.getTraversalSequence(tree.getRoot(), i));
+        }
+    }
+
+    public static void searchAllValues(BinaryTree tree) {
+        // Searches all values in the tree starting from the smallest value
+        // and ending with the largest value, incrementing by 1 each time
+        for (Integer i = tree.getMinTreeValue(), j = 0; i < tree.getMaxTreeValue() + 1; i++) {
+            if (j % 10 == 0) {
+                System.out.println();
+            }
+            System.out.printf("%-5s ", tree.searchTree(i, TraversalMode.RECURSIVE, tree.getRoot()));
+            j++;
         }
 
+    }
+
+    public static BinaryTree getTreeFromInput() {
+        // Grabs a line of user inputted numbers and inserts them into a binary tree
+        Scanner input = new Scanner (System.in);
+        BinaryTree return_tree = new BinaryTree();
+        System.out.println("Enter a sequence of numbers seperated by a comma (i.e 5,4,21,3...)");
+        String[] numbers_string = input.nextLine().split(",");
+
+        for (int i = 0; i < numbers_string.length; i++) {
+            int temp_number = Integer.parseInt(numbers_string[i]);
+            return_tree.insert(temp_number);
+        }
+        input.close();
+        return return_tree;
     }
 }
