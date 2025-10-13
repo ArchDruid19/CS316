93e28f2addbea516b3cf59cbb07dcea16dffb734
ArchDruid19 <druidvester@gmail.com>
2025-10-13 11:13:58 -0400

    More updates

--- Knapsack_Python/main.py
+++ Knapsack_Python/main.py
@@ -1,32 +1,24 @@
 from itertools import combinations
 from knapsack_item import KnapsackItem
-def main():
-    max_knapsack_weight = 7
-    # Create the items that will be in the knapsack with there given names and weights
-    item1 = KnapsackItem("a", 1, 1)
-    item2 = KnapsackItem("b", 3, 4)
-    item3 = KnapsackItem("c", 4, 5)
-    item4 = KnapsackItem("d", 5, 7)
 
-    # Put items in the knapsack
-    knapsack = [item1, item2, item3, item4]
 
-    # Create a list of ((2^n)-1) combinations within the knapsack set (power set)
+def getLargestKnapsackVal(item_list, max_knapsack_weight):
+    # Create a list of ((2^n)-1) combinations within the knapsack set (power set - empty set)
     combinations_list = []
 
-    knapsack_len = len(knapsack)
+    knapsack_idx = len(item_list)
 
     # Append all combinations into a 2D list
-    while knapsack_len > 0:
-        combinations_list.append(list(combinations(knapsack, knapsack_len)))
-        knapsack_len -= 1
+    while knapsack_idx > 0:
+        combinations_list.append(list(combinations(item_list, knapsack_idx)))
+        knapsack_idx -= 1
 
     best_combination = []
     biggest_val = 0
 
     # Go through the combination list and find the total weights and values of each combination,
-    # checking if the total weight is <= the allotted weight. If it is, we keep track of the 
-    # largest value we find and the combination that provided that largest value. 
+    # checking if the total weight is <= the allotted weight. If it is, we keep track of the
+    # largest value we find and the combination that provided that largest value.
     for row in combinations_list:
         for combination in row:
             total_weight = 0
@@ -42,12 +34,28 @@ def main():
                     biggest_val = total_value
                     best_combination = combination
 
-
     print(biggest_val)
 
     for element in best_combination:
         print(element.name)
 
+    return biggest_val
+
+
+def main():
+    # Create a max weight for the knapsack
+    max_weight = 7
+
+    # Create the items that will be in the knapsack with there given names and weights
+    item1 = KnapsackItem("a", 1, 1)
+    item2 = KnapsackItem("b", 3, 4)
+    item3 = KnapsackItem("c", 4, 5)
+    item4 = KnapsackItem("d", 5, 7)
+
+    # Put items in the knapsack
+    item_list = [item1, item2, item3, item4]
+
+    getLargestKnapsackVal(item_list, max_weight)
 
 
 if __name__ == "__main__":
