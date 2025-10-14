a7ae8d96333e39ebd04ea0f9ff84fcdac7fd8b08
ArchDruid19 <druidvester@gmail.com>
2025-10-14 00:08:26 -0400

    More updates

--- Knapsack_Python/main.py
+++ Knapsack_Python/main.py
@@ -3,16 +3,17 @@ from knapsack_item import KnapsackItem
 
 
 def getLargestKnapsackVal(item_list, max_knapsack_weight):
-    # Create a list of ((2^n)-1) combinations within the knapsack set (power set - empty set)
+    # Create a list of ((2^n)-1) combinations within the combinaions list (power set - empty set)
     combinations_list = []
 
     knapsack_idx = len(item_list)
 
-    # Append all combinations into a 2D list
+    # Append the power set into a 2D list
     while knapsack_idx > 0:
         combinations_list.append(list(combinations(item_list, knapsack_idx)))
         knapsack_idx -= 1
 
+    # Create an list that will store the best combination, along with the highest value that was found
     best_combination = []
     biggest_val = 0
 
@@ -24,22 +25,17 @@ def getLargestKnapsackVal(item_list, max_knapsack_weight):
             total_weight = 0
             total_value = 0
             for element in combination:
-                print(element.name, end=" ")
                 total_weight += element.weight
                 total_value += element.value
-            print("w=", total_weight, "v=", total_value, end=" ")
-            print()
             if total_weight <= max_knapsack_weight:
                 if total_value > biggest_val:
                     biggest_val = total_value
                     best_combination = combination
 
-    print(biggest_val)
-
+    print("The largest value that can be stored is:", biggest_val)
+    print("The best combination is:", end=" ")
     for element in best_combination:
-        print(element.name)
-
-    return biggest_val
+        print(element.name, end=" ")
 
 
 def main():
@@ -52,7 +48,7 @@ def main():
     item3 = KnapsackItem("c", 4, 5)
     item4 = KnapsackItem("d", 5, 7)
 
-    # Put items in the knapsack
+    # Put items in an item list
     item_list = [item1, item2, item3, item4]
 
     getLargestKnapsackVal(item_list, max_weight)
