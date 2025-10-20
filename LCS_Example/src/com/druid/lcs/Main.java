6745058e7c3d75dd7914a18e46ac3033aa5a6b03
ArchDruid19 <druidvester@gmail.com>
2025-10-20 08:00:27 -0400

    Updates

--- LCS_Example/src/com/druid/lcs/Main.java
+++ LCS_Example/src/com/druid/lcs/Main.java
@@ -2,24 +2,34 @@ package com.druid.lcs;
 
 public class Main {
     public static void main(String[] args) {
-        String first = "bd";
-        String second ="abcd";
+        String first = "bcd";
+        String second = "abcd";
 
-        int longest = performLCS(first, second, 0, 0);
+        int longest = lcsWrapper(first, second);
 
         System.out.println(longest);
     }
 
-    public static int performLCS(String a, String b, int i, int j) {
-        char[] string_a = a.toCharArray();
-        char[] string_b = b.toCharArray();
-
-        if (i >= string_a.length || j >= string_b.length) {
+    public static int performLCS(char[] a, char[] b, int i, int j) {
+        if (i >= a.length || j >= b.length) {
             return 0;
-        } else if (string_a[i] == string_b[j]) {
+        }
+        System.out.println("a[i] = " + a[i]);
+        System.out.println("i = " + i);
+        System.out.println("b[j] = " + b[j]);
+        System.out.println("j = " + j);
+
+        if (a[i] == b[j]) {
             return 1 + performLCS(a, b, i + 1, j + 1);
         } else {
             return Math.max(performLCS(a, b, i + 1, j), performLCS(a, b, i, j + 1));
         }
+    }
+
+    public static int lcsWrapper(String a, String b) {
+        char[] string_a = a.toCharArray();
+        char[] string_b = b.toCharArray();
+
+        return performLCS(string_a, string_b, 0, 0);
     }
 }
