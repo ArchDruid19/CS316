4c32c1061ed7e141b7156e083aa1cc3144572c9e
ArchDruid19 <druidvester@gmail.com>
2025-11-28 20:10:31 -0500

    Almost done with LCS and Huffman

--- Assingments/Assingment4-PythonVersion/Huffman/main.py
+++ Assingments/Assingment4-PythonVersion/Huffman/main.py
@@ -21,7 +21,6 @@ def createLetterOccurenceDict(string_a):
             if string_a[i] == string_a[j]:
                 letter_occurence += 1
         letter_occurence_dictionary[string_a[i]] = letter_occurence
-    print(letter_occurence_dictionary)
 
     return letter_occurence_dictionary
 
@@ -36,6 +35,9 @@ def createHuffmanTree(letter_occurence_dictionary):
         # Push the new node into a min-heap so we can later create the tree
         heapq.heappush(huffman_min_heap, temp_node)
 
+    # Step 4.
+    # Pop out nodes two at a time and combine them with a parent node and then add that parent node back into
+    # the min-heap
     while len(huffman_min_heap) != 1:
         # Pop out a left node from the heap
         left_node = heapq.heappop(huffman_min_heap)
@@ -54,23 +56,58 @@ def createHuffmanTree(letter_occurence_dictionary):
         # From Huffman Encoding powerpoint pg. 13
         heapq.heappush(huffman_min_heap, parent_node)
 
+    # The last node in the min-heap will be the root of the entire huffman tree
     huffman_tree_root_node = heapq.heappop(huffman_min_heap)
 
+    # Return the root of the huffman tree
     return huffman_tree_root_node
 
 
-def traverseLRV(huffman_root_node):
+def traverseLRV(huffman_root_node, variable_bit_codes_dict, string_result):
+    # Step 5.
+    # construct a dictionary of variable bit length codes using the standard post-order recursive method to traverse
+    # a binary tree
     if huffman_root_node is None:
         return
-    traverseLRV(huffman_root_node.left)
-    traverseLRV(huffman_root_node.right)
-    print(huffman_root_node.frequency, huffman_root_node.character)
+    # If both the left and right node are nothing, then we MUST be at a leaf node (character node)
+    # In this case, we add the character as the key of the dictionary, and the string_result (variable bit code) as the value
+    if huffman_root_node.left is None and huffman_root_node.right is None:
+        variable_bit_codes_dict[huffman_root_node.character] = string_result
+
+    traverseLRV(huffman_root_node.left, variable_bit_codes_dict, string_result + "0")
+    traverseLRV(huffman_root_node.right, variable_bit_codes_dict, string_result + "1")
+
+
+def calculateHuffmanCompression(count_dict, variable_bit_codes_dict, string_a):
+    # Each ASCII character is 8 bits, so multiply the length of the string by 8 to get the original size
+    original_size = len(string_a) * 8
+
+    # We will convert the values of the count dictionary and the variable length bit codes to a list for easier processing in a loop
+    count_dict_values = list(count_dict.values())
+    variable_bit_codes_dict_values = list(variable_bit_codes_dict.values())
+
+    compressed_size = 0
+
+    # Multiply the lengths of the variable bit codes by the number of times they appear in the string to
+    # get the total number of compressed bits (if a letter appears 3 times and has a code length of 2, then it uses 6 total bits; etc...)
+    for i in range(len(count_dict_values)):
+        compressed_size += count_dict_values[i] * len(variable_bit_codes_dict_values[i])
+
+    print(compressed_size / original_size)
+    return compressed_size / original_size
+
+
+def performHuffmanEncoding(string_a):
+    # Function that constructs the huffman encoding
+    count_dict = createLetterOccurenceDict(string_a)
+    huffman_root_node = createHuffmanTree(count_dict)
+    variable_bit_codes_dict = {}
+    traverseLRV(huffman_root_node, variable_bit_codes_dict, "")
+    calculateHuffmanCompression(count_dict, variable_bit_codes_dict, string_a)
 
 
 def main():
-    dict = createLetterOccurenceDict("banana bandana")
-    huffman_root_node = createHuffmanTree(dict)
-    traverseLRV(huffman_root_node)
+    performHuffmanEncoding("saginaw")
 
 
 if __name__ == "__main__":
